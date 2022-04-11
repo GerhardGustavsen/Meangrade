a00e8a42dfdbbb75751b4df2f160e7f8a084891a
Gerhard <gerhargu@stud.ntnu.no>
2022-04-11 13:34:35 +0200

    fetched grades to dash

--- ui/src/main/java/ui/DashboardController.java
+++ ui/src/main/java/ui/DashboardController.java
@@ -1,19 +1,22 @@
 package ui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import core.Grade;
 import core.User;
 
 public class DashboardController {
-
 
   User user;
 
@@ -26,7 +29,6 @@ public class DashboardController {
       GradesController controller = loader.getController();
       controller.passUser(user);
 
-
       Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
       stage.setTitle("Grades");
       stage.setScene(new Scene(root));
@@ -36,11 +38,20 @@ public class DashboardController {
       System.out.print("Did not find data fxml form!");
       e.printStackTrace();
     }
+
+    // Add TRY???
+    ObservableList<String> gradeCollection = FXCollections.observableArrayList();
+    for (Grade grade : user.getGrades()) {
+      String listString = "- " + grade.getCode() + "\t\t\t" + grade.getGrade();
+      gradeCollection.add(listString);
+    }
+
+    ListView<String> listView = new ListView<String>(gradeCollection);
+
   }
 
-
   @FXML
-  private void handleOpenGrades(ActionEvent event){
+  private void handleOpenGrades(ActionEvent event) {
     openGrades(event);
   }
 
